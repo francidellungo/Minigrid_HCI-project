455a203bebc177e94007526e02bf3fa4abc67381
francesca <francidl96@gmail.com>
2019-12-19 09:22:38 +0100

    scrollable areas, possibility to add items

---
+++ Ui_newGame.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'newGame.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_new_game_Dialog(object):
+    def setupUi(self, new_game_Dialog):
+        new_game_Dialog.setObjectName("new_game_Dialog")
+        new_game_Dialog.setWindowModality(QtCore.Qt.NonModal)
+        new_game_Dialog.setEnabled(True)
+        new_game_Dialog.resize(495, 359)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(new_game_Dialog.sizePolicy().hasHeightForWidth())
+        new_game_Dialog.setSizePolicy(sizePolicy)
+        new_game_Dialog.setTabletTracking(False)
+        new_game_Dialog.setAcceptDrops(False)
+        new_game_Dialog.setAutoFillBackground(False)
+        new_game_Dialog.setSizeGripEnabled(False)
+        new_game_Dialog.setModal(False)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(new_game_Dialog)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.game_label = QtWidgets.QLabel(new_game_Dialog)
+        self.game_label.setObjectName("game_label")
+        self.verticalLayout_2.addWidget(self.game_label)
+        self.game_buttonBox = QtWidgets.QDialogButtonBox(new_game_Dialog)
+        self.game_buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.game_buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Save)
+        self.game_buttonBox.setCenterButtons(False)
+        self.game_buttonBox.setObjectName("game_buttonBox")
+        self.verticalLayout_2.addWidget(self.game_buttonBox)
+
+        self.retranslateUi(new_game_Dialog)
+        self.game_buttonBox.accepted.connect(new_game_Dialog.accept)
+        self.game_buttonBox.rejected.connect(new_game_Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(new_game_Dialog)
+
+    def retranslateUi(self, new_game_Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        new_game_Dialog.setWindowTitle(_translate("new_game_Dialog", "New Game"))
+        self.game_label.setText(_translate("new_game_Dialog", "image game "))
+
--- minigrid_GUI.py
+++ minigrid_GUI.py
@@ -1,27 +1,98 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, qApp
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QLabel, QPushButton, QHBoxLayout
 from PyQt5.QtGui import QIcon, QPixmap
+# from Ui_mainWindow import Ui_MainWindow
 
 from Ui_minigrid import Ui_MainWindow
 from Ui_newGame import Ui_new_game_Dialog
+from Ui_main_scrollbar import Ui_MainWindow
+
+# class MainWindow_(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.ui = Ui_MainWindow()
+#         self.ui.setupUi(self)
+#         pixmap = QPixmap('games/MiniGrid-Empty-6x6-v0/2019-12-11_13:40:14/game1.png')
+#
+#         self.ui.image_1.setPixmap(pixmap)
+#         self.ui.image_2.setPixmap(pixmap)
+#         horiz_l1 = QHBoxLayout()
+#         label1 = QLabel('label1')
+#         b1 = QPushButton('b1')
+#         horiz_l1.addWidget(label1)
+#         horiz_l1.addWidget(b1)
+#
+#         self.ui.verticalLayout.addLayout(horiz_l1)
+#         # connect button New Game
+#         self.new_game_Dialog = NewGame()
+#         # self.num_games = 2
+#         # self.ui.new_game_button.clicked.connect(lambda: self.new_game_Dialog.exec_())
+#         self.ui.new_game_button.clicked.connect(lambda: self.add_row('row1'))
+#
+#     def add_row(self, name):
+#         horiz = QHBoxLayout()
+#         label = QLabel(name)
+#         info_button = QPushButton('info')
+#         move_button = QPushButton('->')
+#         horiz.addWidget(label)
+#         horiz.addWidget(info_button)
+#         horiz.addWidget(move_button)
+#         self.ui.verticalLayout_games.addLayout(horiz)
 
 
-class MainWindow_(QMainWindow):
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.ui = Ui_MainWindow()
+#         self.ui.setupUi(self)
+#         self.games_items = []
+#         self.ranking_items = []
+#         # for each game create a row and add to the tree
+#
+#         label = QLabel()
+#         label.setText('game 1')
+#         button = QPushButton()
+#         button.setText('info')
+#         image_1 = [label, 'image1', button, 'move ->']
+#         strings = ['ciao', 'dadas', 'dddd']
+#         l = []  # list of QTreeWidgetItem to add
+#         for i in strings:
+#             l.append(QTreeWidgetItem([i]))  # create QTreeWidgetItem's and append them
+#         # tree.addTopLevelItems(l)
+#
+#         self.ui.tree_games.addTopLevelItems(QTreeWidgetItem(image_1))
+#
+#         # item = QTreeWidgetItem()
+#         self.ui.tree_games.addTopLevelItems(l)
+#
+
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+
         pixmap = QPixmap('games/MiniGrid-Empty-6x6-v0/2019-12-11_13:40:14/game1.png')
-
-        self.ui.image_1.setPixmap(pixmap)
-        self.ui.image_2.setPixmap(pixmap)
-
         # connect button New Game
         self.new_game_Dialog = NewGame()
-        # self.num_games = 2
-        self.ui.new_game_button.clicked.connect(lambda: self.new_game_Dialog.exec_())
+        # self.ui.new_game_pb.clicked.connect(lambda: self.new_game_Dialog.exec_())
+        # ly = self.ui.games_verticalLayout
+        self.ui.new_game_pb.clicked.connect(lambda: self.add_row('row1'))
 
+    def add_row(self, name):
+        horiz = QHBoxLayout()
+        label = QLabel(name)
+        info_button = QPushButton('info')
+        move_button = QPushButton('->')
+        horiz.addWidget(label)
+        horiz.addWidget(info_button)
+        horiz.addWidget(move_button)
+        self.ui.games_verticalLayout.addLayout(horiz)
 
 class NewGame(QDialog):
     def __init__(self, **kwargs):
@@ -30,11 +101,8 @@ class NewGame(QDialog):
         self.ui.setupUi(self)
 
 
-
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow_()
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
