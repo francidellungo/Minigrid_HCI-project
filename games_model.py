73db1973f196c1732f1dc6b5451b52ada9e63b23
francesca <francidl96@gmail.com>
2020-01-17 15:14:49 +0100

    save new game fixed, arrows imgs

--- games_model.py
+++ games_model.py
@@ -70,9 +70,6 @@ class GamesModel(QObject):
 
         self.game_moved.emit(dest_list_name, folder_name)
 
-
-        # TODO emit signal list changed
-
     def remove_game(self, folder_name, list_):
         """
         remove a game from a list
